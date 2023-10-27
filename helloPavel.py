998314d19d6c00209fe111277f6139afc9eae9fe
Pavel Polkanov <63466418+PaulPolkanov@users.noreply.github.com>
2023-10-27 22:58:40 +0300

    add new branch and add new function pavel

--- helloPavel.py
+++ helloPavel.py
@@ -1,2 +1,9 @@
 print('Hello Pavel')
 print('Hello again')
+print('Hello again')
+print('Hello again')
+print('Hello again')
+print('Hello again')
+def pavel():
+    print('it is C++!!!!!!!!!!')
+pavel()
